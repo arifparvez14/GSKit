c8d60b9b9d2db06603c586aaaca22c9bf0a89442
arifparvez14 <arif.mbstu22@gmail.com>
2021-08-26 17:30:52 +0600

    First commit

---
+++ Carthage/Build/GSKit.xcframework/ios-arm64_x86_64-simulator/GSKit.framework/Headers/GSKit.h
@@ -0,0 +1,18 @@
+//
+//  GSKit.h
+//  GSKit
+//
+//  Created by ShareTrip iOS on 26/8/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GSKit.
+FOUNDATION_EXPORT double GSKitVersionNumber;
+
+//! Project version string for GSKit.
+FOUNDATION_EXPORT const unsigned char GSKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GSKit/PublicHeader.h>
+
+
---
+++ GSKit/GSViewController.swift
@@ -0,0 +1,14 @@
+//
+//  GSViewController.swift
+//  GSKit
+//
+//  Created by ShareTrip iOS on 26/8/21.
+//
+
+import UIKit
+
+open class GSViewController: UIViewController {
+    public func changeBackground(withColor color: UIColor) {
+        self.view.backgroundColor = color
+    }
+}
